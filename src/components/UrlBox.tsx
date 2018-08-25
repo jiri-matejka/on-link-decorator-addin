136b4967a17f556f61ab50d292a5f01971bb5484
Jiří Matějka <jura.matejka@gmail.com>
2018-08-25 13:03:01 +0200

    Remove UrlBox intrinsic state

--- src/components/UrlBox.tsx
+++ src/components/UrlBox.tsx
@@ -6,70 +6,24 @@ export interface UrlBoxProps {
 	isUrlValid: boolean
 }
 
-interface UrlBoxState {
-	faviconUrl: string,
-	faviconData: string,
-	isLoading: boolean,
-	isObtained: boolean
-	isInvalidUrl: boolean,
-	isHttpError: boolean
-}
 
-export class UrlBox extends React.Component<UrlBoxProps, UrlBoxState> {
+export class UrlBox extends React.Component<UrlBoxProps> {
     constructor(props, context) {
         super(props, context);
-        this.state = {
-			faviconUrl: null,
-			faviconData: null,
-			isLoading: false,
-			isObtained: false,
-			isInvalidUrl: false,
-			isHttpError: false
-		};
-		
+      
 		this.onTextFieldBlur = this.onTextFieldBlur.bind(this);
 		this.render = this.render.bind(this);
-		this.setStatusLoadingFailed = this.setStatusLoadingFailed.bind(this);
 	}
-
-
 
 	render() {
 		
 		return (
-			<TextField label="Insert your hyperlink then click Paste" key="txtbox" 
+			<TextField label="Url" key="txtbox" 
 				errorMessage={!this.props.isUrlValid ? "Url not valid" : null} 
 				onBlur={this.onTextFieldBlur} />
 		)
 	}
 		
-
-	pickFavicon(faviconRecords) {
-		if(faviconRecords.icons.length == 0)
-			return null;
-	
-		let icons32 = faviconRecords.icons.filter(function (icon) { return icon.sizes === "32x32"; });
-	
-		if(icons32.length > 0)
-			return icons32[0].src;
-		else
-			return faviconRecords.icons[0]; // anything
-	
-	}
-
-
-
-	setStatusLoadingFailed() {
-		this.setState({
-			faviconUrl: null,
-			faviconData: null,
-			isLoading: false,
-			isObtained: false,
-			isInvalidUrl: false,
-			isHttpError: true
-		});
-	}
-
 	onTextFieldBlur(newValue: React.FocusEvent<HTMLElement>) {		
 		const url = (newValue.currentTarget as HTMLInputElement).value;
 
