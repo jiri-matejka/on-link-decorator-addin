35b01fb3a4876671a9f70c32562f2cf01b909fad
Jiří Matějka <jura.matejka@gmail.com>
2018-08-25 12:51:22 +0200

    Fix problem with Preview.isVisible prop

--- src/containers/PreviewContainer.ts
+++ src/containers/PreviewContainer.ts
@@ -1,14 +1,14 @@
 import { connect } from 'react-redux'
 
 import { IAppState, FetchState } from '../stateDefinition'
-import { IPreviewStaticProps, IPreviewDispatchProps } from '../components/Preview'
+import { IPreviewStaticProps } from '../components/Preview'
 import Preview from '../components/Preview'
 
-function mapDispatchToProps(dispatch) : IPreviewDispatchProps {
-	return {
+// function mapDispatchToProps(dispatch) : IPreviewDispatchProps {
+// 	return {
 		
-	};
-}
+// 	};
+// }
 
 function mapStateToProps(state : IAppState) : IPreviewStaticProps {
 	return {
@@ -16,12 +16,12 @@ function mapStateToProps(state : IAppState) : IPreviewStaticProps {
 		faviconData: state.faviconData,
 		faviconMime: state.faviconMime,
 		isLoading: state.fetchState === FetchState.FETCH_IN_PROGRESS,
-		isVisible: state.fetchState === FetchState.NOT_STARTED,
+		isVisible: state.fetchState !== FetchState.NOT_STARTED,
 		isErrored: state.fetchState === FetchState.FETCH_ERROR,
-		title : state.title,
+		title : state.title === null ? "" : state.title,
 		error: state.fetchError
 	};
 }
 
-export const PreviewContainer = connect(mapStateToProps, mapDispatchToProps)(Preview);
+export const PreviewContainer = connect(mapStateToProps)(Preview);
 
